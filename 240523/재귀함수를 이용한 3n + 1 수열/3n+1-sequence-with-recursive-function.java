178f80c2882ac1eb09c3acfd73ac7ca66c240993
hiharim <72755537+hiharim@users.noreply.github.com>
2024-05-23 19:42:59 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240523/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
+++ 240523/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -7,21 +7,19 @@ public class Main {
         
         int n= Integer.parseInt(br.readLine());
 
-        System.out.println(F(n));
+        System.out.println(countNum(n));
 
     }
 
-    public static int F(int n){
-        int cnt = 0;
+    public static int countNum(int n){
+        if(n==1)
+            return 0;
 
-        while(n>1){
-            cnt++;   
-            if(n%2==0)
-                n=n/2;
-            else
-                n=3*n+1;
-        }
-       
-        return cnt;
+
+        if(n%2==0)
+            return countNum(n/2)+1;
+        else
+            return countNum(3*n+1) +1;
+
     }
 }
