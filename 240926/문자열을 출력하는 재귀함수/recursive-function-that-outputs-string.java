10f6d4ce593b9b5b18d8d9d4f3737dcfc1512a88
hiharim <72755537+hiharim@users.noreply.github.com>
2024-09-26 18:02:22 +0900

    Title: 문자열을 출력하는 재귀함수, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240926/문자열을 출력하는 재귀함수/recursive-function-that-outputs-string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        while(cnt<n){
+            System.out.println("Coding is my favorite hobby!");
+            cnt++;
+        }
+    }
+}
